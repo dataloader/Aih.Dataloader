e438bb53edefcf63ee9409f408685a6ed64e9e46
Arnþór Ingi Hinriksson <arnthor.hinriksson@gmail.com>
2016-06-10 11:32:21 +0000

    failed test to test tests on builderver

--- tests/TestDataLoaderTester.cs
+++ tests/TestDataLoaderTester.cs
@@ -38,7 +38,7 @@ namespace tests
             loader.RunDataLoader();
 
             bool callCountOK = loader.CallCount == 5;
-            bool configCountOK = loader.ConfigCount == 7;
+            bool configCountOK = loader.ConfigCount == 8;
 
             Assert.True(callCountOK && configCountOK);
         }
